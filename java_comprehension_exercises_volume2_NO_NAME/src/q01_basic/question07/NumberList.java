c0f31f8766d7541e1a04338db76c9eb223e8660e
yuiosa <yuiosa@C010423044.zipangu.cac.co.jp>
2026-04-30 16:32:28 +0900

    q01_basic.question07 終了

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question07/NumberList.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question07/NumberList.java
@@ -22,7 +22,9 @@ class NumberList {
 	 */
 	public void addFromOneTo(Integer limit) {
 		//TODO ここから実装する
-		numbers.add(limit);
+		for (int i = 1; i < limit + 1; i++) {
+			numbers.add(i);
+		}
 	}
 
 	/**
@@ -33,11 +35,12 @@ class NumberList {
 	public Integer calcSumOfList() {
 		Integer sum = 0;
 		//TODO ここから実装する
-		for (int i = 1; i < numbers.size(); i++) {
-			sum += i;
-		}
+		for (int i = 0; i < numbers.size(); i++) {
 
+			sum += numbers.get(i);
+		}
 		return sum;
+
 	}
 
 	/**
@@ -46,8 +49,9 @@ class NumberList {
 	 */
 	public void doubleListEachValue() {
 		//TODO ここから実装する
-		for (int i = 1; i < numbers.size(); i++) {
-			i *= 2;
+		for (int i = 0; i < numbers.size(); i++) {
+			numbers.set(i, numbers.get(i) * 2);
+
 		}
 
 	}
@@ -58,10 +62,11 @@ class NumberList {
 	 */
 	public void removeIndexOfFirstHalf() {
 		//TODO ここから実装する
-		for (int i = 1; i < numbers.size(); i++) {
-			numbers.remove(numbers.size() / 2);
-		}
+		int num = (numbers.size() / 2);
+		for (int i = 0; i < num; i++) {
 
+			numbers.remove(0);
+		}
 	}
 
 	/**
--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question07/SystemMain07.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question07/SystemMain07.java
@@ -1,8 +1,11 @@
 package q01_basic.question07;
 
+import java.io.IOException;
+import java.util.List;
+
 public class SystemMain07 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		NumberList numberList = new NumberList();
 		System.out.println("1-任意の整数をListに代入します");
@@ -15,26 +18,27 @@ public class SystemMain07 {
 		} catch (NumberFormatException e) {
 			// TODO: handle exception
 			e.printStackTrace();
-
-			System.out.println("****************");
-			System.out.println("Listの合計を計算します");
-			int sum = 0;
-			//TODO ここから実装する
-			numberList.addFromOneTo(limit);
-
-			System.out.println("1から" + limit + "までの合計は" + sum + "です。");
-
-			System.out.println("****************");
-			System.out.println("Listの各要素を2倍します");
-			//TODO ここから実装する
-			numberList.getNumbers();
-			System.out.println();
-
-			System.out.println("****************");
-			System.out.println("Listの前半分のインデックスの要素を削除します");
-			//TODO ここから実装する
-
 		}
+		System.out.println("****************");
+		System.out.println("Listの合計を計算します");
+		int sum = 0;
+		//TODO ここから実装する
+		numberList.addFromOneTo(limit);
+
+		System.out.println("1から" + limit + "までの合計は" + sum + "です。");
+
+		System.out.println("****************");
+		System.out.println("Listの各要素を2倍します");
+		//TODO ここから実装する
+		List<Integer> numbers = numberList.getNumbers();
+		System.out.println(numbers);
+
+		System.out.println("****************");
+		System.out.println("Listの前半分のインデックスの要素を削除します");
+		//TODO ここから実装する
+		numberList.removeIndexOfFirstHalf();
+		System.out.println(numberList.getNumbers());
 
 	}
+
 }
