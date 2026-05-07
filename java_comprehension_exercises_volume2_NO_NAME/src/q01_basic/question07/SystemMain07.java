c0d3406176cadd2284af2fd5fcc9287fb618b0b9
yuiosa <yuiosa@C010423044.zipangu.cac.co.jp>
2026-05-07 10:26:28 +0900

    q01_basic.question07 終了

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question07/SystemMain07.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question07/SystemMain07.java
@@ -1,7 +1,6 @@
 package q01_basic.question07;
 
 import java.io.IOException;
-import java.util.List;
 
 public class SystemMain07 {
 
@@ -12,26 +11,31 @@ public class SystemMain07 {
 		System.out.print("input number>>");
 		int limit = 0;
 		//TODO ここから実装する
+
 		try {
 			ConsoleReader consoleReader = new ConsoleReader();
-			consoleReader.inputNumber();
+			limit = consoleReader.inputNumber();
+
 		} catch (NumberFormatException e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
+		System.out.println(numberList.getNumbers());
+
 		System.out.println("****************");
 		System.out.println("Listの合計を計算します");
 		int sum = 0;
 		//TODO ここから実装する
 		numberList.addFromOneTo(limit);
+		sum = numberList.calcSumOfList();
 
 		System.out.println("1から" + limit + "までの合計は" + sum + "です。");
 
 		System.out.println("****************");
 		System.out.println("Listの各要素を2倍します");
 		//TODO ここから実装する
-		List<Integer> numbers = numberList.getNumbers();
-		System.out.println(numbers);
+		numberList.doubleListEachValue();
+		System.out.println(numberList.getNumbers());
 
 		System.out.println("****************");
 		System.out.println("Listの前半分のインデックスの要素を削除します");
